821818d568d31539d3e56d902fcdd757f39a4b3c
Dmitry Pokidov <dooman87@gmail.com>
2019-03-19 06:07:12 +1100

    Using cloudformation. WIP

--- test-env.sh
+++ test-env.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-aws --endpoint-url=http://localhost:4572 s3api create-bucket --bucket images
+#aws --endpoint-url=http://localhost:4581 cloudformation create-stack --stack-name infra --template-body file://infra.yaml --region us-east-1
+#aws --endpoint-url=http://localhost:4572 s3 cp lambda/index.zip s3://code
+#aws --endpoint-url=http://localhost:4581 cloudformation create-stack --stack-name infra --template-body file://lambda.yaml --region us-east-1
 
+aws --endpoint-url=http://localhost:4572 s3api create-bucket --bucket images
 aws --endpoint-url=http://localhost:4572 s3api create-bucket --bucket code
 aws --endpoint-url=http://localhost:4572 s3 cp lambda/index.zip s3://code
 aws --endpoint-url=http://localhost:4574 lambda create-function --function-name invalidate-cdn --runtime nodejs8.10 --code S3Bucket=code,S3Key=index.zip --role arn:aws:iam::123456:role/role-name --handler index.handler --region us-east-1
