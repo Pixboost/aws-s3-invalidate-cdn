f07661b2ceffe298bdd4efa9b0ac6c33b8b55319
Dmitry Pokidov <dooman87@gmail.com>
2019-04-19 08:18:33 +1000

    Integration tests. WIP.

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,17 +4,18 @@ const fs = require('fs');
 
 describe('test', () => {
   before((done) => {
-    const s3 = AWS.S3({
-      endpoint: 'http://localstack:4572'
+    const s3 = new AWS.S3({
+      endpoint: new AWS.Endpoint('http://localhost:4572'),
+      s3ForcePathStyle: true
     });
 
     const uploadParams = {
       Bucket: 'images',
       Key: 'image.png',
-      Body: fs.createReadStream('image.png')
+      Body: fs.readFileSync('./image.png'),
     };
-    s3.upload(uploadParams, (err) => {
-      if (!err) {
+    s3.putObject(uploadParams, (err) => {
+      if (err) {
         throw err;
       }
 
